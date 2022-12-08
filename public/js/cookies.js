4dd736d8a5efb88bb176d88f1c88f1cccb4bbfae
ameyagholkar <ameya.moses@gmail.com>
2022-12-08 14:36:48 -0800

    Update to add cookies from API

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,58 +1,80 @@
 function getCookies() {
-    document.getElementById("cookies").innerHTML = document.cookie;
-    if (!cookieStore) {
-        document.getElementById("cookieStore").innerHTML = "Cookie Store API not supported";
-        return;
+  document.getElementById("cookies").innerHTML = document.cookie;
+  if (!cookieStore) {
+    document.getElementById("cookieStore").innerHTML =
+      "Cookie Store API not supported";
+    return;
+  }
+
+  cookieStore.getAll().then((cookies) => {
+    if (cookies.length === 0) {
+      document.getElementById("cookieStore").innerHTML = "No cookies";
+      return;
     }
+    document.getElementById("cookieStore").innerHTML = JSON.stringify(
+      cookies,
+      null,
+      2
+    );
+  });
+}
 
-    cookieStore.getAll().then(cookies => {
-        if (cookies.length === 0) {
-            document.getElementById("cookieStore").innerHTML = "No cookies";
-            return;
-        }
-        document.getElementById("cookieStore").innerHTML = JSON.stringify(cookies, null, 2);
-    });
-  }
-  
-  function setCookiesFromApi() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        getCookies();
-      }
-    };
-    xhr.open("GET", "/set");
-    xhr.send();
-  }
-
-  function setCookiesFromClient() {
-    const key = document.getElementById("key")?.value;
-    const cookieValue = document.getElementById("cookieValue")?.value;
-    const secure = document.getElementById("secure")?.checked;
-    const sameSite = document.getElementById("sameSite")?.value;
-    const partitioned = document.getElementById("partitioned")?.checked;
-
-    let cookie = `${encodeURIComponent(key)}=${encodeURIComponent(cookieValue)};max-age=${30*24*60*60}`;
-    cookie += secure ? ";Secure" : "";
-    cookie += sameSite ? `;SameSite=${sameSite}` : "";
-    cookie += partitioned ? ";Partitioned" : "";
-
-    document.cookie = cookie;
-
-    getCookies();
-  }
-
-  function clearCookies() {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i];
-      var eqPos = cookie.indexOf("=");
-      var name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
-      document.cookie = name + "=;SameSite=None;Secure;Partitioned;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-      document.cookie = name + "=;SameSite=None;Secure;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-      document.cookie = name + "=;SameSite=Non;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-      document.cookie = name + "=;Secure;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+function setCookiesFromApi() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      getCookies();
     }
-    getCookies();
+  };
+  xhr.open("GET", "/cookies/samesite/set");
+  xhr.send();
+}
+
+function setPartitionedCookiesFromApi() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      getCookies();
+    }
+  };
+  xhr.open("GET", "/cookies/partitioned/set");
+  xhr.send();
+}
+
+function setCookiesFromClient() {
+  const key = document.getElementById("key")?.value;
+  const cookieValue = document.getElementById("cookieValue")?.value;
+  const secure = document.getElementById("secure")?.checked;
+  const sameSite = document.getElementById("sameSite")?.value;
+  const partitioned = document.getElementById("partitioned")?.checked;
+
+  let cookie = `${encodeURIComponent(key)}=${encodeURIComponent(
+    cookieValue
+  )};max-age=${30 * 24 * 60 * 60}`;
+  cookie += secure ? ";Secure" : "";
+  cookie += sameSite ? `;SameSite=${sameSite}` : "";
+  cookie += partitioned ? ";Partitioned" : "";
+
+  document.cookie = cookie;
+
+  getCookies();
+}
+
+function clearCookies() {
+  var cookies = document.cookie.split(";");
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    var eqPos = cookie.indexOf("=");
+    var name = (eqPos > -1 ? cookie.substring(0, eqPos) : cookie).trim();
+    document.cookie =
+      name +
+      "=;SameSite=None;Secure;Partitioned;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie =
+      name + "=;SameSite=None;Secure;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie =
+      name + "=;SameSite=Non;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = name + "=;Secure;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
   }
+  getCookies();
+}
--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var serveStatic = require('serve-static');
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var serveStatic = require("serve-static");
 
 var app = express();
 app.use(cookieParser());
-app.use(serveStatic(__dirname + '/public'));
+app.use(serveStatic(__dirname + "/public"));
 
-app.get('/set', (req, res) => {
-  res.cookie('3pcookie', 'value', { sameSite: 'none', secure: true });
-  res.cookie('3pcookie-legacy', 'value', { secure: true });
-  res.cookie('3pcookie-insecure', 'value', { sameSite: 'none' });
-  res.cookie('3pcookie-insecure-legacy', 'value');
+app.get("/cookies/samesite/set", (req, res) => {
+  res.cookie("3pcookie", "value", { sameSite: "none", secure: true });
+  res.cookie("3pcookie-legacy", "value", { secure: true });
+  res.cookie("3pcookie-insecure", "value", { sameSite: "none" });
+  res.cookie("3pcookie-insecure-legacy", "value");
+  res.end();
+});
+
+app.get("/cookies/partitioned/set", (req, res) => {
+  res.setHeader("Set-Cookie", [
+    "__Host-partitioned=DELETE FROM DEVTOOLS; SameSite=None; Secure; Path=/; Partitioned",
+    "__Host-partitioned-lax=DELETE FROM DEVTOOLS; SameSite=Lax; Secure; Path=/; Partitioned",
+    "__Host-partitioned-strict=DELETE FROM DEVTOOLS; SameSite=Strict; Secure; Path=/; Partitioned",
+    "__Host-not-partitioned=DELETE FROM DEVTOOLS; SameSite=None; Secure; Path=/",
+  ]);
+
   res.end();
 });
 
 var port = process.env.port || 3000;
 app.listen(port, function () {
-  console.log('Listening on http://localhost:' + port);
+  console.log("Listening on http://localhost:" + port);
 });
